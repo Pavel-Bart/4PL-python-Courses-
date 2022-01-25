30b23007c64e1b7ed8e497f5989293b16f316252
Pavel-Bart <pavel.bart3@gmail.com>
2022-01-25 20:00:03 +0200

    1-pask

---
+++ 1-pask/main.py
@@ -0,0 +1,22 @@
+URL = "https://"
+
+name = "Pavel Bartosevic"
+age = 18
+
+# print(name.upper())
+# print(name.isupper())
+# print(name.isalnum())
+# print(name.isalpha())
+# print(name.lower())
+# print(name.islower())
+
+# print("My name is: " + name)
+# print(f"My name is {name}")
+# print("My age is: " + str(age))
+# print(f"My age is {age}")
+
+print(f"""
+My name is {name}
+My age is {age}""")
+
+print(f"My name is {name}\nMyage is {age}\n")
