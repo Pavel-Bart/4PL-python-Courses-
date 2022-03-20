9943f140fb818ed6e526b70920c3f8060983fb53
Pavel-Bart <pavel.bart3@gmail.com>
2022-03-20 16:15:02 +0200

    15 pask

--- 14-pask/turtle-race/main.py
+++ 14-pask/turtle-race/main.py
@@ -1,9 +1,11 @@
 import turtle as t
+from turtle import Turtle, Screen
 from random import randint
 
-screen = t.Screen()
-screen.setup(500, 500, 0, 0)
 
+screen = t.Screen()
+screen.setup(520, 400, 0, 0)
+# screen.screensize(canvwidth=510, canvheight=510, bg="blue")
 
 tim = t.Turtle()
 tim.shape("turtle")
@@ -16,22 +18,58 @@ joe.color("red")
 tom = tim.clone()
 tom.color("blue")
 
-tim.goto(-250, 0)
-joe.goto(-250, 50)
-tom.goto(-250, 100)
+tick = tim.clone()
+tick.color("black")
 
-tim.speed(2)
-joe.speed(1)
-tim.pendown()
-joe.pendown()
+tack = tim.clone()
+tack.color("yellow")
 
-tim.forward(499)
-joe.forward(482)
+tim.setpos(-250, 0)
+joe.setpos(-250, 50)
+tick.setpos(-250, -50)
+tom.setpos(-250, 100)
+tack.setpos(-250, -100)
 
 
-def random_speed():
-    speed = randint(0, 10)
-    return speed
+user_bet = screen.textinput("MB", "Make a bet:")
+is_race_on = True
 
+
+with open("score.txt") as file:
+    score_data = file.readline().strip().split(" ")
+    scoreName = score_data[0]
+    scoreNum = int(score_data[1])
+
+    data = file.readlines()
+    colors = []
+    wins = []
+    for x in data:
+        content = x.strip().split(" ")
+        colors.append(content[0])
+        wins.append(int(content[1]))
+
+while is_race_on:
+    for turtle in screen.turtles():
+        if turtle.xcor() > 230:
+            is_race_on = False
+            winner_color = turtle.pencolor()
+
+            if user_bet == winner_color:
+                print(f"Your turtle won!")
+            else:
+                print(f"Sorry! Won {winner_color} turtle")
+
+        random_distance = randint(1, 10)
+        turtle.forward(random_distance)
+
+for i in range(0, 5):
+    if winner_color == colors[i]:
+        wins[i] += 1
+
+with open("score.txt", mode="w") as file:
+    scoreNum += 1
+    file.write(f"{scoreName} {scoreNum}\n")
+    for i in range(0, 5):
+        file.write(f"{colors[i]} {wins[i]}\n")
 
 screen.exitonclick()
