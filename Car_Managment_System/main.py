61b3260b9474d6033afb00fe1f15ce5b232ea202
Pavel-Bart <pavel.bart3@gmail.com>
2022-06-07 18:30:51 +0200

    crush

---
+++ Car_Managment_System/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from database import engine
+from routers import student_routers
+
+import models
+
+app = FastAPI()
+app.include_router(student_routers.router)
+app.include_router(class_routers.router)
+app.include_router(boss_routers.router)
+
+models.Base.metadata.create_all(engine)
---
+++ Car_Managment_System/models.py
@@ -0,0 +1,7 @@
+from database import Base
+from sqlalchemy import Column, String, Integer, ForeignKey, Boolean, DATE, Float, DateTime
+from sqlalchemy.orm import relationship
+
+
+
+
